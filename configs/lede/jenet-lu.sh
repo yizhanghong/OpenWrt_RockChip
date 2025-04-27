a7fba1e4bfc4540c4ac981319608305462f5867b
yizhanghong <865362414@qq.com>
2025-04-27 11:53:16 +0800

    Update jenet-lu.sh

--- configs/lede/jenet-lu.sh
+++ configs/lede/jenet-lu.sh
@@ -1,2 +1,5 @@
 echo '修改主机名'
 sed -i "s/hostname='OpenWrt'/hostname='JENET'/g" ./package/base-files/files/bin/config_generate
+echo '修改dts'
+rm -rf target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-opc-h69k.dts
+cp -f $GITHUB_WORKSPACE/configs/lede/jenet-lu.dts target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3568-opc-h69k.dts

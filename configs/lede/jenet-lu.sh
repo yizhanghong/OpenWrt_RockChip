8d61c4d22d43ae139e7ec26655b831c4bc0bd4f4
yizhanghong <865362414@qq.com>
2025-04-27 12:23:52 +0800

    Update jenet-lu.sh

--- configs/lede/jenet-lu.sh
+++ configs/lede/jenet-lu.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#=================================================
+# File name: jenet-lu.sh
+# System Required: Linux
+# Version: 1.0
+# Lisence: MIT
+# Author: SuLingGG
+# Blog: https://mlapp.cn
+#=================================================
 echo '修改主机名'
 sed -i "s/hostname='OpenWrt'/hostname='JENET'/g" ./package/base-files/files/bin/config_generate
 echo '修改dts'
